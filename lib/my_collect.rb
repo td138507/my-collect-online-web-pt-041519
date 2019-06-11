00638f752495fc3d9e1022cbeef074c5030d9189
Taylor Doyle <tcd56@cornell.edu>
2019-06-11 20:35:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,19 @@
 def my_collect(new_collection)
   i = 0
+  new_collection = []
   while i < new_collection.length
-    yield(array[i])
+    new_collection << yield(array[i])
     i += 1
   end
   new_collection
 end
 
-names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-
-my_collect(names) do |names|
-  puts names.split(" ").first
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
 end
 
-languages = ['ruby', 'javascript', 'python', 'objective-c']
-
-my_collect(languages) do |languages|
-  puts languages.upcase
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
 end
