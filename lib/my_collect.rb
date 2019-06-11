f25cf3cd9bbdd8338f203eee33551c28413cd61b
Taylor Doyle <tcd56@cornell.edu>
2019-06-11 19:20:13 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
+def my_collect(array)
+  i = 0
+  while i < array.length
+    yield(array[i])
+    i += i 
+  end
+  array
+end
+
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
 
 
