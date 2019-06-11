70260cdd647bc4a8554243238db7c93874a8012e
Taylor Doyle <tcd56@cornell.edu>
2019-06-11 20:23:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -8,11 +8,13 @@ def my_collect(array)
 end
 
 names = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+
 my_collect(names) do |name|
   puts name.split(" ").first
 end
 
 languages = ['ruby', 'javascript', 'python', 'objective-c']
+
 my_collect(languages) do |language|
   puts language.upcase
 end
